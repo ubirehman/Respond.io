5a6add37764e462d5e859bc61f18600cc8666ed9
Ubaid Ur Rehman <rehmanubaid5@gmail.com>
2025-05-01 17:07:09 +0500

    Created notes factory and nodtes type

--- factory/NoteFactory.js
+++ factory/NoteFactory.js
@@ -1,6 +1,11 @@
+const PersonalNote = require("../note/PersonalNote")
+const WorkNote = require("../note/WorkNote")
+const { NOTE: TYPE } = require('../utils/constants')
 
 class NoteFactory {
     static createInstance(type) {
+        if (type === TYPE.PERSONAL) return new PersonalNote();
+        if (type === TYPE.WORK) return new WorkNote();
         throw new Error("Note type not recognized");
     }
 }
